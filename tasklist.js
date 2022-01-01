e4b549f3c5f2a3c740e7df22745c903c9894377f
Yasin ÇABUK <yasincabuk16@gmail.com>
2022-01-01 06:05:26 +0300

    Added proper comments line by line on the js file.

--- tasklist.js
+++ tasklist.js
@@ -1,20 +1,27 @@
+//Here is our variables and a function
+//to check the input value length for the exact moment that event triggered.
 const addTaskButton = document.getElementById("add-task-btn");
 const taskInput = document.getElementById("task-input");
 const taskLength = () => taskInput.value.length;
 
+//This is our main function that adds items to the ul list.
 const addTaskItem = () => {
+
     if (taskLength() > 0 ) {
-        const newtaskItem = document.createElement("li");
-        newtaskItem.classList.add("tasklist-item");
-        newtaskItem.appendChild(document.createTextNode(taskInput.value));
-        document.querySelector("ul").appendChild(newtaskItem);
-        taskInput.value = "";
+        const newtaskItem = document.createElement("li"); //Defining the new task item as a variable.
+        newtaskItem.classList.add("tasklist-item"); //adding a class to that new item.
+        newtaskItem.appendChild(document.createTextNode(taskInput.value)); //getting and setting the new item text.
+        document.querySelector("ul").appendChild(newtaskItem); //appending the item to the DOM.
+        taskInput.value = ""; //cleaning the input so there wont be infinite adding by pressing enter.
 }
 }
-addTaskButton.addEventListener("click", addTaskItem);
-taskInput.addEventListener("keypress", function(e){
-    console.log(e.key);
+
+//adding an option to add items via Enter key.
+const addTaskviaEnter = (e) => {
     if (e.which == 13 || e.key == "Enter") {
         addTaskItem();
     }
-});
+}
+//finally adding our event listeners and connecting our predefined functions.
+addTaskButton.addEventListener("click", addTaskItem);
+taskInput.addEventListener("keypress", addTaskviaEnter);
